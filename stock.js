ccd1b17aea2d44c357f2bebd0ce1a4f77d4fdab4
web-tserver <graphinkvisualcreative@gmail.com>
2026-02-03 11:01:18 +0800

    Add files via upload

---
+++ stock.js
@@ -0,0 +1,17 @@
+// FAZGAME STOCK DATA
+export const STOCK = [
+  {
+    n: 'PlayStation 5 Disc',
+    c: 'console',
+    type: 'console',
+    p: 2399,
+    cond: 'new'
+  },
+  {
+    n: 'RTX 3060',
+    c: 'pc',
+    type: 'pc',
+    p: 1299,
+    cond: 'used'
+  }
+];
